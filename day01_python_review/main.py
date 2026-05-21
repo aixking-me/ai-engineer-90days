22ba1d61bf0991f5e7201468201d497dc378f1dc
wjk <3076954738@qq.com>
2026-05-22 07:14:36 +0800

    02_cg

--- day01_python_review/main.py
+++ day01_python_review/main.py
@@ -2,8 +2,11 @@ name = input("输入你的名字")
 age = int(input("输入你的年龄"))
 
 print(f"你好，{name},你今年{age}岁。")
-while age >= 18:
-    print("你已经成年")
-    break
-else:
+if age < 0:
+    print("年龄不能是负数")
+elif age < 18:
     print("你还未成年")
+elif age >= 18 and age < 60:
+    print("你已经成年")
+else :
+    print("你已经可以退休了")
